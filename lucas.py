e346f807257e3607b1a03cf0e1d27ad84f48a729
LuMuM <105711426+LuMuM@users.noreply.github.com>
2022-05-18 11:37:12 +0200

    Update lucas.py

--- lucas.py
+++ lucas.py
@@ -1,5 +1,13 @@
 nom = "lucas"
-print(nom)
-for i in nom :
-    print(i)
 
+def backwards( nom ) :
+    j = len(nom)
+    k = 1
+    while k <= j: 
+        print(nom[j-k])
+        k += 1
+
+
+backwards(nom)
+
+    
