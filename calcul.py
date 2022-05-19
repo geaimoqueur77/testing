2c43e5382913cdb9d18fabd6fa08ca1132102ceb
Ben <saynemail@gmail.com>
2022-05-19 10:56:31 +0200

    removed files

--- calcul.py
+++
@@ -1,9 +0,0 @@
-x = 1
-y = 2
-a = x + y   
-b = x - y
-c = x * y
-d = x / y 
-e = x % y
-f = x**y
-print(a,b,c,d,e,f)
--- dino.py
+++
@@ -1,6 +0,0 @@
-dino=[2,6]
-print(type(dino))
-dino=8
-print(type(dino))
-dino="salut"
-print(type(dino))
