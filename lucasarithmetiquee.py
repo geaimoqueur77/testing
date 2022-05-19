ca5770df158861288ac69ef61806057a93b9b515
LuMuM <105711426+LuMuM@users.noreply.github.com>
2022-05-19 11:58:43 +0200

    fix

---
+++ lucasarithmetiquee.py
@@ -0,0 +1,32 @@
+
+from sympy import Range
+
+
+Lucas = list()
+bool = True
+print(Lucas)
+Lucas.append("lasagne")
+Lucas.append(1)
+Lucas.append(bool)
+print(Lucas)
+
+Onepiece = ["luffy", "zoro", "sanji", "nami", "chopper", "robin", "franky", "jinbei"]
+print(Onepiece)
+ 
+Notestephane = [20, 20, 20, 20]
+
+print(Notestephane)
+
+Phrase = ["J'ai", 20, "ans"]
+print(Phrase)
+
+Count = list(range(20))
+print(Count)
+
+
+Count = list(range(0,50, 3))
+print(Count)
+
+print(len(Count))
+
+print(Count[len(Count)-1])
