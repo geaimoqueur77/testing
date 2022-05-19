8b07cc97f5e6e39baf3f05ad382c5496a13bade6
dounya-bourhani <bourhani.dounya@gmail.com>
2022-05-19 10:41:08 +0200

    Update dounya.py

--- dounya.py
+++ dounya.py
@@ -1,9 +1,21 @@
 import numpy as np
 
-print("Dounya")
+#print("Dounya")
 
+"""
 a = 7
 print(a)
 print(type(a))
 a = "coucou"
 print(type(a))
+"""
+
+a = 1+2
+b = 2-3
+c = 3*4
+d = 4/5
+e = 11 % 2
+f = 2**3
+
+print(a, b, c, d, e, f)
+
