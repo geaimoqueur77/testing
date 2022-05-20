5e9007cf28a23005c47d2828611b0dc05db430a9
geaimoqueur77 <ghalieai77@gmail.com>
2022-05-20 11:34:14 +0200

    exos 20 05 2022

---
+++ Ghali/Tableaux.py
@@ -0,0 +1,8 @@
+from pandas import DataFrame as df
+import pandas as pd
+import numpy as np
+tableau = pd.DataFrame(np.array([[1,2,3],[4,5,6],[7,8,9]]))
+print(tableau)
+
+#Fonctions
+from pandas import read_csv
