9ae7681c41423f7f7e4bcd3b88bb10e2c1238982
Votre joli nom <yufei.li@univ-lyon2.fr>
2022-05-20 10:01:12 +0200

    Update panda.py

--- Li/panda.py
+++ Li/panda.py
@@ -1,13 +1,5 @@
 #import
 from pandas import DataFrame as df
-import panda as pd 
+import pandas as pd 
 
-#Par indices
-df.iloc[0,5]
-df.iloc[0:5,0:3]
-df.iloc[:,0:3]
-df.loc[:,'NomColonne']
 
-#Par position
-df.head(5)
-df.head(10)
