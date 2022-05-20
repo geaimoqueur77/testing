fab16d30cca18d889e708b15c22d38c7eee8d050
Votre joli nom <yufei.li@univ-lyon2.fr>
2022-05-20 10:24:53 +0200

    Update panda.py

--- Li/panda.py
+++ Li/panda.py
@@ -3,3 +3,18 @@ from pandas import DataFrame as df
 import pandas as pd 
 
 
+#Par Indices
+import numpy as np
+tableau = pd.DataFrame(np.array([[1,2,3],[4,5,6],[7,8,9]]))
+print(tableau)
+print(tableau.iloc[1,2])
+print(tableau.iloc[0:1,0:1])
+print(tableau.iloc[0:2, : ]) #methode 1
+print(tableau.iloc[0;2,0:3]) #methode 2
+
+print(tableau.head(1))
+
+
+
+
+
