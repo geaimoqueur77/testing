a220acddcbee1d90a38acf1a9d62211c0a64b6f6
Antoine19999 <anthonysedoh@gmail.com>
2022-05-20 10:00:16 +0200

    Update tuto.py

--- Antoine/tuto.py
+++ Antoine/tuto.py
@@ -5,8 +5,6 @@ Created on Fri May 20 09:24:15 2022
 
 @author: antoinesedoh
 """
-
-
 #On créee une liste nommée NomListe
 NomListe = list()
 
@@ -17,8 +15,6 @@ NomListe.append(3)
 liste1= ["dinosaure", "pancakes"] # Liste de mots
 liste2= [1, 2, 3] # Liste de chiffres
 liste3= [17, "pirouette", 3, "cacahouète"] # Liste mixte
-liste4= range(10) # Liste de chiffres de 0 à 9
-liste5= range(1, 10, 2) # Liste de chiffres de 1 à 10 en allant de 2 en 2
 
 #On crée une liste de liste avec les liste 1, 2 et 3. 
 MegaListe = [liste1, liste2, liste3]
@@ -27,15 +23,28 @@ MegaListe = [liste1, liste2, liste3]
 print ("Le contenu de la liste est : ",liste1)
 print ("Le contenu de la liste est : ",liste2)
 print ("Le contenu de la liste est : ", liste3)
-print ("Le contenu de la liste est : ",liste4)
-print ("Le contenu de la liste est : ",liste5)
+
 
 #Pour pouvoir aérer l'affichage
+print("")
 print("")
 
 # On affiche la longueur de la liste
 print ("La longueur de la liste est : ", len(liste1))
 print ("La longueur de la liste est : ", len(liste2))
 print ("La longueur de la liste est : ", len(liste3))
-print ("La longueur de la liste est : ", len(liste4))
-print ("La longueur de la liste est : ", len(liste5))
+
+
+
+print("")
+print("")
+Element = MegaListe[2][0] #3ème liste, 1er élément (l'index commence à 0)
+print("Le premier élément de la troisième liste est : ", Element)
+
+#On trie la liste grâce à la méthode sort ()
+
+liste6 = ["a","d","m", "e", "k", "g"]
+print (liste6)
+print("")
+liste6.sort()
+print ("La liste triée est : ", liste6)
