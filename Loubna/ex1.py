92ee27527ea7778678e651198ad3d783a644bee0
loubna zidan <89811760+Loubna123321@users.noreply.github.com>
2022-05-21 19:20:47 +0200

    Delete ex1.py

--- Loubna/ex1.py
+++
@@ -1,7 +0,0 @@
-
-def mot() :
-    mot = str(input("entrer stop pour arreter : "))
-    while mot != "stop" :        
-        mot = str(input("entrer stop pour arreter : "))#redemander d'entrer un mot
-    print("Fin: sortir de la boucle")
-mot()
