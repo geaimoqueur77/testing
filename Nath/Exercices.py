142d6eb1e4a06c11f12e9292645b52546fa2d02d
NathanaelRR <nathanael.r.r@gmail.com>
2022-05-22 23:56:33 +0200

    Exos_Inachevés_Nathanael

---
+++ Nath/Exercices.py
@@ -0,0 +1,22 @@
+#Exo 0
+def Fonction1(a,b) :
+    if   1 <= a <= 10 :
+        x= a**b
+        print(x)
+
+Fonction1(10,3)
+
+#Exo 1
+#input(x)
+    #if x == "stop":
+        #stopper l'execution
+
+#Exo 2
+
+#Exo 3 
+
+#Exo 4
+
+
+
+
