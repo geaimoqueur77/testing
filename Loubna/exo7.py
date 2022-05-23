edc8f21e6093d4a92abc183931922920e6aa446f
Loubna <loubna.zidan0000@hotmail.com>
2022-05-23 20:54:27 +0200

    Update exo7.py

--- Loubna/exo7.py
+++ Loubna/exo7.py
@@ -5,14 +5,11 @@ def multiple(intervalle):
     for i in intervalle:
         if i % 3 == 0 and i % 5 != 0:
             print ("fizz")
-        if i % 5 == 0 and i % 3 != 0:
+        elif i % 5 == 0 and i % 3 != 0:
            print ("buzz")
-        if i % 3 == 0 and i % 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
              print ("fizzbuzz")
         else:
             print(i)
 
-
-
-
-print("la sortie est :", multiple(intervalle))
+multiple(intervalle)
