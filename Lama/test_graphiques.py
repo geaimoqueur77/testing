180ff1c80a3b7cc45a1a8d5217fed31e271f84e7
Lama <lamazidan123@gmail.com>
2022-05-23 22:26:12 +0200

    Create test_graphiques.py

---
+++ Lama/test_graphiques.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+from pandas import read_csv 
+data=pd.read_csv('C:\\Users\\Lama\MonProjet\\Exercice1.csv', header=0, index_col=0 , sep=',' , decimal='.')
+print(data)
+
+#-------------------#
+
+
+from matplotlib import pyplot as plt
+plt.plot(data, color='yellow', linestyle = ':',marker = 'o', markersize = 10) # Créer une courbe
+plt.ylabel('taille')
+plt.xlabel('age')
+plt.title('Courbe des ages et tailles')
+plt.grid(True)
+plt.show() #Afficher la courbe
+
+#--------------------#
+
+
+plt.hist(data, bins = 5, color = 'skyblue') # Créer un histogramme
+plt.ylabel('taille')
+plt.xlabel('age')
+plt.title('Histogramme des ages et tailles')
+plt.show() # Afficher un histogramme
+
+
+
+
