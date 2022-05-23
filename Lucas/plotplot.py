7c9233d2f63707085e8d91353adefbed440f425e
LuMuM <105711426+LuMuM@users.noreply.github.com>
2022-05-23 12:06:14 +0200

    Does this work

---
+++ Lucas/plotplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('Lucas/Exercice1.csv',header = 0, index_col = 0, sep = ',', decimal = '.')
+
+plt.plot(data, "b--", scalex = True, scaley = True)
+plt.xlabel('Age', family='serif',color='b',weight='normal', size = 16,labelpad = 6)
+plt.ylabel('Taille', family='serif',color='r',weight='normal', size = 16,labelpad = 6)
+plt.title("Tailles en fonction de l'age", fontdict={'family': 'serif', 'color' : 'darkblue','weight': 'bold','size': 18})
+plt.grid(True)
+plt.show()
