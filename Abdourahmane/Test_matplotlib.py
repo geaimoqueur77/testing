01bfb10623e808566781612703c579ef756f333e
Abdouragit <105711497+Abdouragit@users.noreply.github.com>
2022-05-24 11:02:50 +0200

    part 2 - Manipulation de données - matplotlib + stats descriptives

--- Abdourahmane/Test_matplotlib.py
+++ Abdourahmane/Test_matplotlib.py
@@ -4,6 +4,7 @@ import matplotlib as mplt
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 data = pd.read_csv("Abdourahmane\Exercice1.csv",header=0,sep=',',decimal='.')
 #on peut ajouter "index_col = 0" pour dire que la première colonne numérote les individus
@@ -55,7 +56,37 @@ print(np.mean(data['taille']))
 
 # print(np.mean(data['age''taille'])) #ceci ne fonctionne guère
 
+print() #les médianes
+print("Mediane : ",np.median(data))
+
+print("Mediane : ",np.median(data['age']))
+print("Mediane : ",np.median(data['taille']))
 
 
 
+print() #les variances
+print(np.var(data['age']))
+print(np.var(data['taille']))
 
+print() #les écarts-types
+print(np.std(data['age']))
+print(np.std(data['taille']))
+
+
+print() #les pourçentiles
+# print(np.percentile(data)) #cette chose ne fonctionne point, il faut préciser
+
+print(np.percentile(data,25))
+
+print(np.percentile(data['age'],25))
+print(np.percentile(data['taille'],25))
+
+
+"""
+#usage de scipy
+print("lili")
+stats.mode(data) # cette fonction de sipy nous renvoi le mode d'une distribution
+stats.mode(data['age'])
+stats.mode(data['taille'])
+
+"""
