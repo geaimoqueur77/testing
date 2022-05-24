0dbf1722b236044d01df614f4dc2b13f6b7aa430
Votre joli nom <yufei.li@univ-lyon2.fr>
2022-05-24 11:24:59 +0200

    Create Statistiques inférentielles.py

---
+++ Li/Statistiques inférentielles.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+data = pd.read_csv("Li/Exercice1.csv",header=0,sep= ',', decimal='.')
+
+## Tests de normalité
+# Test de Shapiro-Wilk
+print()
+from scipy.stats import shapiro
+print("shapiro avec age: ",shapiro(data['age']),"shapiro avec taille: ",shapiro(data['taille']))
+print("'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+## Tests de corrélation
+# Test de Pearson
+from scipy.stats import pearsonr
+import numpy as np
+
+x=np.array(data['age'])
+y=np.array(data['taille'])
+
+ps = pearsonr(x,y)
+print("Test de Pearson: ",ps)
+print("'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+# Test de Spearman
