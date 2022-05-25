4e166855435429726261ce997ccbf0f6f7eb95d9
NathanaelRR <nathanael.r.r@gmail.com>
2022-05-25 09:18:41 +0200

    Rectange_Nath

---
+++ Nath/Rectangle.py
@@ -0,0 +1,22 @@
+
+def Dimension_Rectangle(longueur,largeur):
+    i=0
+    j=0
+    a=['_']
+    b=['_']
+
+    while  j != largeur:
+        j=j+1
+        a.append('_')
+    print(a)
+
+    while  i != longueur:
+        i=i+1
+        print(b)
+
+    while  j != largeur:
+        j=j+1
+        a.append('_')
+    print(a)
+
+Dimension_Rectangle(5,5)    
