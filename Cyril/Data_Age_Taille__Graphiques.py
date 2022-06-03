3e8b20b9dd7b863f19ad97c86e852f3db2e6a60d
Cyr <cyril.kocab@orange.fr>
2022-06-03 14:13:00 +0200

    Added correction to several exercices

--- Cyril/Data_Age_Taille__Graphiques.py
+++ Cyril/Data_Age_Taille__Graphiques.py
@@ -3,14 +3,24 @@ import pandas as pd
 
 # matplotlib.use("Qt5Agg")
 
-data = pd.read_csv('../Exo_Python/3_Exploration_données/Données.csv')
+data = pd.read_csv('../../../Exo_Python/3_Exploration_données/Données.csv', sep=',', decimal=".")
+# Importation du fichier de données, avec "," en séparateur, et "." en décimal
 
-x = data.iloc[:, 0]
-y = data.iloc[:, 1]
+x = data.iloc[:, 0]  # Récupère toutes les lignes de la colonne d'index 0 (donc ici l'âge)
+y = data.iloc[:, 1]  # Récupère toutes les lignes de la colonne d'index 1 (donc ici la taille)
 
 # --------------------
 
-plt.scatter(x, y, color="green")
+plt.scatter(x, y, c="green")  # Nuage de points de y (taille) en fonction de x (âge). Points de couleur verte
+plt.xlabel('âge')  # Label de l'abscisse
+plt.ylabel('taille')  # Label de l'ordonnée
+plt.grid(True)  # Tracé d'un quadrillage
+plt.show()  # Fonction obligatoire pour afficher le graphique.
+# Jusque là, ce n'était que de l'initialisation des différents paramètres
+
+# --------------------
+
+plt.bar(x, y, color="green")  # Diagramme à barres
 plt.xlabel('âge')
 plt.ylabel('taille')
 plt.grid(True)
@@ -18,7 +28,7 @@ plt.show()
 
 # --------------------
 
-plt.bar(x, y, color="green")
+plt.hist(x, bins=5, color="green")  # Histogramme
 plt.xlabel('âge')
 plt.ylabel('taille')
 plt.grid(True)
@@ -26,15 +36,7 @@ plt.show()
 
 # --------------------
 
-plt.hist(x, bins = 5, color="green")
-plt.xlabel('âge')
-plt.ylabel('taille')
-plt.grid(True)
-plt.show()
-
-# --------------------
-
-plt.plot(x, y, color="green")
+plt.plot(x, y, color="green")  # Courbe reliant les points (sans les points
 plt.xlabel('âge')
 plt.ylabel('taille')
 plt.grid(True)
--- Cyril/Data_Age_Taille__StatsInfer.py
+++ Cyril/Data_Age_Taille__StatsInfer.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from scipy.stats import shapiro, pearsonr, spearmanr, chi2_contingency, ttest_ind, f_oneway, wilcoxon, mannwhitneyu
 
-data = pd.read_csv('../Exo_Python/3_Exploration_données/Données.csv')
+data = pd.read_csv('../../../Exo_Python/3_Exploration_données/Données.csv')
 
 # x = data.iloc[:,0]
 # y = data.iloc[:,1]
 
-x = data['age']  # Ou x = data.age
-y = data['taille']
+x = data['age']     # Ou x = data.age
+y = data['taille']  # Ou y = data.taille
 
 # # # Tests de normalité # # #
 # ----- Test de Shapiro -----
@@ -17,24 +17,24 @@ print(f"Test de Shapiro : {shapiro(data)}")
 # # # Tests de corrélation # # #
 # ----- Test de Pearson ----- : lien d'évolution entre des variables
 
-print(f"Test de Pearson : {pearsonr(x,y)}")
+print(f"Test de Pearson : {pearsonr(x, y)}")
 
 # ----- Test de Spearman ----- : lien d'évolution entre des variables basé sur les rangs plutôt que les valeurs
 
-print(f"Test de Spearman : {spearmanr(x,y)}")
+print(f"Test de Spearman : {spearmanr(x, y)}")
 
 # ----- Test du Khi2 ----- : variables qualitatives dépendantes
 
-print(f"Test du Khi2 : {chi2_contingency(data,data)}")
+print(f"Test du Khi2 : {chi2_contingency(data, data)}")
 
 # # # Tests paramétriques # # #
 # ----- Test de Student ----- : relation entre deux échantillons liés
 
-print(f"Test de Student : {ttest_ind(data,data)}")
+print(f"Test de Student : {ttest_ind(data, data)}")
 
 # ----- Test ANOVA ----- : relation entre plusieurs échantillons liés
 
-print(f"Test ANOVA : {f_oneway(data,data)}")
+print(f"Test ANOVA : {f_oneway(data, data)}")
 
 # # # Tests non-paramétriques # # #
 # ----- Test de Mann-Whitney ----- : distribution de deux petits échantillons indépendants
@@ -43,6 +43,6 @@ print(f"Test ANOVA : {f_oneway(data,data)}")
 print(f"Test de Mann-Whitney : {(stat, p)}")
 
 # ----- Test de Wilcoxon ----- : distribution de deux petits échantillons indépendants,
-                                # basé sur leur rang plutôt que leur valeur
+# basé sur leur rang plutôt que leur valeur
 
 print(f"Test de Wilcoxon : {wilcoxon(x,y)}")
